a25cd1c51baf501720f30de1ff523d9f719b3e46
daxter-army <mehulsaini456@gmail.com>
2023-03-19 17:35:41 +0530

    Added responsive functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, lazy, Suspense } from 'react';
 import ThemeContext from './context/theme';
+
+import Loader from './pages/Loader/Loader';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home/Home';
-import Works from './pages/Works/Works';
-import Posts from './pages/Posts/Posts';
-
 import './App.css';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Works = lazy(() => import('./pages/Works/Works'));
+// const Posts = lazy(() => import('./pages/Posts/Posts'));
 
 function App() {
   const [theme, setTheme] = useState('light')
@@ -32,9 +34,9 @@ function App() {
       <HashRouter>
         <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/works' element={<Works />} />
-          <Route path='/posts' element={<Posts />} />
+          <Route path='/' element={<Suspense fallback={<Loader />}><Home /></Suspense>} />
+          <Route path='/works' element={<Suspense fallback={<Loader />}><Works /></Suspense>} />
+          {/* <Route path='/posts' element={<Suspense fallback={<div>loading</div>}><Posts /></Suspense>} /> */}
         </Routes>
       </HashRouter>
       <Footer />
