46d18cabbc99584c6a1f6a602e5471a7890c86c2
Mehul Singh Teya <mehulsaini456@gmail.com>
2023-12-28 23:40:15 +0530

    feat: aria-label for <Button /> same as text

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,22 +1,31 @@
-import "./Button.css"
+import "./Button.css";
 
 interface ButtonProps {
-	btnClicker: () => void;
-	text: string;
-	rightIcon: JSX.Element;
-	isCenter?: boolean;
-	styles?: React.CSSProperties;
+  btnClicker: () => void;
+  text: string;
+  rightIcon: JSX.Element;
+  isCenter?: boolean;
+  styles?: React.CSSProperties;
 }
 
-const Button = ({ btnClicker, text, rightIcon, isCenter = false, styles }: ButtonProps) => {
-	return (
-		<button aria-label="action-button" className={`buttonWpr ${isCenter ? 'isCenter' : ''}`} style={styles} onClick={btnClicker}>
-			<span>
-				{text}
-			</span>
-			{rightIcon}
-		</button>
-	)
-}
+const Button = ({
+  btnClicker,
+  text,
+  rightIcon,
+  isCenter = false,
+  styles,
+}: ButtonProps) => {
+  return (
+    <button
+      aria-label={text}
+      className={`buttonWpr ${isCenter ? "isCenter" : ""}`}
+      style={styles}
+      onClick={btnClicker}
+    >
+      <span>{text}</span>
+      {rightIcon}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
